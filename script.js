f55519c2ad30a06bc542a5d81ba6b45b7fd41403
Guss Rw <gussrw1@gmail.com>
2021-01-21 11:29:21 -0600

    update audios

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-button.addEventListener('click', function () {
-    please.play()
+button.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.getAttribute("data-dblclick") == null) {
+        el.setAttribute("data-dblclick", 1);
+        setTimeout(function () {
+            if (el.getAttribute("data-dblclick") == 1) {
+                plase.play();
+            }
+            el.removeAttribute("data-dblclick");
+        }, 300);
+    } else {
+        el.removeAttribute("data-dblclick");
+        thanks.play();
+    }
+})
+
+button.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
+    iam.play()
 })
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(res => {
